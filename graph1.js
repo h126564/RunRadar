5444ce2182b5a4d48965da3badf6df58b55af3a0
h126564 <80750283+h126564@users.noreply.github.com>
2024-11-08 09:42:06 +0100

    added graph2 which has a hard coded API response to create the graph drawing code with

--- graph1.js
+++ graph1.js
@@ -1,10 +1,11 @@
 function graph1(p) {
     p.setup = function () {
-      p.createCanvas(window.innerWidth-100, 200);
-      p.background(0);
+      p.createCanvas(window.innerWidth, 200);
+      p.background(color("#212121"));
     };
     p.draw = function () {
       p.circle(p.mouseX, p.mouseY, 50);
     };
   };
   new p5(graph1);
+
