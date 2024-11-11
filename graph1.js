eb07ab515b9dd35c350d12dcd61fb8e7dd8538ce
h126564 <80750283+h126564@users.noreply.github.com>
2024-11-11 13:12:58 +0100

    fixed too big for screen

--- graph1.js
+++ graph1.js
@@ -1,6 +1,6 @@
 function graph1(p) {
     p.setup = function () {
-      p.createCanvas(window.innerWidth, 200);
+      p.createCanvas(window.innerWidth-20, 200);
       p.background(p.color("#212121"));
     };
     p.draw = function () {
