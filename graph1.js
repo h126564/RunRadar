32cd1cef04afaa098eb3990a09d03c28cbb3b5b3
h126564 <80750283+h126564@users.noreply.github.com>
2024-11-11 13:09:28 +0100

    denk p5 gefixt???

--- graph1.js
+++ graph1.js
@@ -1,8 +1,7 @@
 function graph1(p) {
-  colorMode(RGB)
     p.setup = function () {
       p.createCanvas(window.innerWidth, 200);
-      p.background(color("#212121"));
+      p.background(p.color("#212121"));
     };
     p.draw = function () {
       p.circle(p.mouseX, p.mouseY, 50);
