b80a02a086a5d2ae59f77f98b8b9e8edce90b0c4
h126564 <80750283+h126564@users.noreply.github.com>
2024-11-13 09:39:10 +0100

    test api response

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const input = document.getElementById("locationfield");
-const autocomplete = new google.maps.places.Autocomplete(input, options);
+const searchButton = document.getElementById("searchButton");
+let apiResponse = {};
+searchButton.onclick = function(){
+    // Define the API URL
+    
+    const apiUrl = 'https://geocode.maps.co/search?q=' + input.value  + '&api_key=67346201ecee5360511634fte9d92c5';
+
+    // Make a GET request
+    fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        apiResponse=data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+};
