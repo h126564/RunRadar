fa453f218b37e983bbbd25c7a288fa6b72609295
immensenotary648 <152868088+immensenotary648@users.noreply.github.com>
2024-11-29 08:25:45 +0100

    Pagina 3 changes

--- Pagina_3_website_weer_2024_11_27_08_08_13/sketch.js
+++ Pagina_3_website_weer_2024_11_27_08_08_13/sketch.js
@@ -1,63 +1,66 @@
-let icons
-let temperature = 6
+let icons;
+let temperature = 6;
 let windsnelheid = 21;
 
-function setup() {
-  createCanvas(1900, 972);
-  background("#222831");
-  updateData()
+P.setup = function() {
+  P.createCanvas(1900, 972);
+  P.background("#222831");
+  updateData();
 }
 
-function preload() {
-  windrichting = loadImage("direction.png");
+P.preload = function() {
+  windrichting = P.loadImage("direction.png");
 }
 
-function mousePressed() {
-  fullscreen(true);
+P.mousePressed = function() {
+  P.fullscreen(true);
 }
 
-function draw() {
+P.draw = function() {
   zoekscherm();
   kaart();
   bestetijd();
 }
 
 function zoekscherm() {
-  fill("#393E46");
-  rect(50, 50, 850, 400, 50);
-  fill('white');
-  textSize(50);
-  text("wat is het beste weer tussen:", 70, 100);
+  P.fill("#393E46");
+  P.rect(50, 50, 850, 400, 50);
+  P.fill('white');
+  P.textSize(50);
+  P.text("wat is het beste weer tussen:", 70, 100);
 }
 
 function kaart() {
-  fill("#393E46");
-  rect(1000, 50, 850, 850, 50);
+  P.fill("#393E46");
+  P.rect(1000, 50, 850, 850, 50);
 }
 
 function bestetijd() {
-  fill("#393E46");
-  rect(50, 500, 850, 400, 50);
-  fill("#222831")
-  rect(400, 550, 450, 300, 20)
-  image(icons, 420, 550, 150, 150)
-  textAlign(CENTER);
-  fill('white')
-  text(`${temperature}°C`, 510, 750);
-  text(`${windsnelheid} km/h`, 730, 750);
-    push();
-    angleMode(DEGREES);
-    translate(720, 630);
-    rotate(windgraden);
-    imageMode(CENTER);
-    image(windrichting, 0, 0, 100, 100);
-    pop();
-  textAlign(LEFT);
+  P.fill("#393E46");
+  P.rect(50, 500, 850, 400, 50);
+  P.fill("#222831")
+  P.rect(400, 550, 450, 300, 20);
+  P.image(icons, 420, 550, 150, 150);
+  P.textAlign(P.CENTER);
+  P.fill('white');
+  P.text(`${temperature}°C`, 510, 750);
+  P.text(`${windsnelheid} km/h`, 730, 750);
+
+  P.push();
+  P.angleMode(P.DEGREES);
+  P.translate(720, 630);
+  P.rotate(windgraden);
+  P.imageMode(P.CENTER);
+  P.image(windrichting, 0, 0, 100, 100);
+  P.pop();
+
+  P.textAlign(P.LEFT);
 }
 
 function updateData() {
   const weatherIconCode = "04n";
-    icons = loadImage(  `https://rodrigokamada.github.io/openweathermap/images/${weatherIconCode}_t.png`
-    );
+  icons = P.loadImage(
+    `https://rodrigokamada.github.io/openweathermap/images/${weatherIconCode}_t.png`
+  );
   windgraden = 30;
 }
