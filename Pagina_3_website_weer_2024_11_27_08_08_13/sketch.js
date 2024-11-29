2a8fd3e514f9cc428213d133e519953fafb4c9b0
immensenotary648 <152868088+immensenotary648@users.noreply.github.com>
2024-11-29 08:30:43 +0100

    .

--- Pagina_3_website_weer_2024_11_27_08_08_13/sketch.js
+++ Pagina_3_website_weer_2024_11_27_08_08_13/sketch.js
@@ -1,69 +1,70 @@
-function pagina3(p){
-let icons;
-let temperature = 6;
-let windsnelheid = 21;
+function pagina3(p) {
+  let icons;
+  let temperature = 6;
+  let windsnelheid = 21;
 
-P.setup = function() {
-  P.createCanvas(1900, 972);
-  P.background("#222831");
-  updateData();
+  p.setup = function() {
+    p.createCanvas(1900, 972);
+    p.background("#222831");
+    updateData();
+  }
+
+  p.preload = function() {
+    windrichting = p.loadImage("direction.png");
+  }
+
+  p.mousePressed = function() {
+    p.fullscreen(true);
+  }
+
+  p.draw = function() {
+    zoekscherm();
+    kaart();
+    bestetijd();
+  }
+
+  function zoekscherm() {
+    p.fill("#393E46");
+    p.rect(50, 50, 850, 400, 50);
+    p.fill('white');
+    p.textSize(50);
+    p.text("wat is het beste weer tussen:", 70, 100);
+  }
+
+  function kaart() {
+    p.fill("#393E46");
+    p.rect(1000, 50, 850, 850, 50);
+  }
+
+  function bestetijd() {
+    p.fill("#393E46");
+    p.rect(50, 500, 850, 400, 50);
+    p.fill("#222831")
+    p.rect(400, 550, 450, 300, 20);
+    p.image(icons, 420, 550, 150, 150);
+    p.textAlign(p.CENTER);
+    p.fill('white');
+    p.text(`${temperature}°C`, 510, 750);
+    p.text(`${windsnelheid} km/h`, 730, 750);
+
+    p.push();
+    p.angleMode(p.DEGREES);
+    p.translate(720, 630);
+    p.rotate(windgraden);
+    p.imageMode(p.CENTER);
+    p.image(windrichting, 0, 0, 100, 100);
+    p.pop();
+
+    p.textAlign(p.LEFT);
+  }
+
+  function updateData() {
+    const weatherIconCode = "04n";
+    icons = p.loadImage(
+      `https://rodrigokamada.github.io/openweathermap/images/${weatherIconCode}_t.png`
+    );
+    windgraden = 30;
+  }
 }
 
-P.preload = function() {
-  windrichting = P.loadImage("direction.png");
-}
-
-P.mousePressed = function() {
-  P.fullscreen(true);
-}
-
-P.draw = function() {
-  zoekscherm();
-  kaart();
-  bestetijd();
-}
-
-function zoekscherm() {
-  P.fill("#393E46");
-  P.rect(50, 50, 850, 400, 50);
-  P.fill('white');
-  P.textSize(50);
-  P.text("wat is het beste weer tussen:", 70, 100);
-}
-
-function kaart() {
-  P.fill("#393E46");
-  P.rect(1000, 50, 850, 850, 50);
-}
-
-function bestetijd() {
-  P.fill("#393E46");
-  P.rect(50, 500, 850, 400, 50);
-  P.fill("#222831")
-  P.rect(400, 550, 450, 300, 20);
-  P.image(icons, 420, 550, 150, 150);
-  P.textAlign(P.CENTER);
-  P.fill('white');
-  P.text(`${temperature}°C`, 510, 750);
-  P.text(`${windsnelheid} km/h`, 730, 750);
-
-  P.push();
-  P.angleMode(P.DEGREES);
-  P.translate(720, 630);
-  P.rotate(windgraden);
-  P.imageMode(P.CENTER);
-  P.image(windrichting, 0, 0, 100, 100);
-  P.pop();
-
-  P.textAlign(P.LEFT);
-}
-
-function updateData() {
-  const weatherIconCode = "04n";
-  icons = P.loadImage(
-    `https://rodrigokamada.github.io/openweathermap/images/${weatherIconCode}_t.png`
-  );
-  windgraden = 30;
-}
-}
-new p5(pagina3)
+new p5(pagina3);
